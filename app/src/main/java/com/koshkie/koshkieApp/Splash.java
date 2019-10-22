c5f81c4c3dc44b0b4c8e505847b990f7cfa27ccf
Ahmed Emad <ahmed39652003@gmail.com>
2019-10-22 18:56:56 +0200

    Added Splash Screen

---
+++ app/src/main/java/com/koshkie/koshkieApp/Splash.java
@@ -0,0 +1,26 @@
+package com.koshkie.koshkieApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(Splash.this, MainActivity.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, 2000);
+    }
+}
